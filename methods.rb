93453a8b476dee6208db2da378b377794c3e9569
Bob Oyier <boboyier92@gmail.com>
2023-02-03 20:52:52 +0300

    Completed Methods lab.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer()
+	puts "Hello, programmer!"
+end
+
+def greet(param)
+	puts "Hello, #{param}!"
+end
+
+def greet_with_default(param = "programmer")
+	puts "Hello, #{param}!"
+end
+
+def add(a,b)
+	return a + b
+end
+
+=begin 
+
+def halve(param)
+	return param/2
+end
+
+=end
+
+def halve(param)
+	if param == Integer
+		return param/2
+	else
+		return nil
+end
